568dfecc8df7c55fbb4b2bc604d0bd2a7864219f
Desean Richard <desean@Deseans-MacBook-Air.local>
2024-03-04 14:31:14 -0600

    JavaVaul

---
+++ jsvault.js
@@ -0,0 +1,30 @@
+// Create a string for the creator saying they have received this message
+//Assign three variables that equal or exact the result of the three given codes in the combination
+// The First Number needs to equal 10
+// The Second Number needs to equal 40
+// The Third Number needs to equal 39
+//Add comments to explain the process in each of the code to explain more
+// Make a dialog / alert box showing the vault codes 
+//
+
+
+
+
+
+
+console.log(" You have received this message because you have been chosen to open an important vault. Here is the secret combination");
+
+
+const product = 5 * 2; //multiplication of the first combination which equals 10 
+
+console.log(product, "I am the product and First Combination");
+
+
+const quotient = 80 / 2; //division of the second combination which equals 40
+
+console.log(quotient, " I am the quotient and the Second Combination");
+
+const num = 10 + 29;  // num being the third number being to the combination
+console.log (num, " I am the Third Number of the combination");
+
+alert ("First Number is 10. Second Number is 40. Third Number is 39.");
---
+++ main.js
@@ -0,0 +1,11 @@
+let numCows = 50;
+
+
+const x = "Hello World!";
+console.log("Hello World!")
+
+console.log("Hello + DisneyWorld!");
+
+const numUmberllas= 10 * 3;
+
+
